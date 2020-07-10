e1247d2cc56e4b05fa634bbdafc2a7b6a81e2021
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-07-10 15:34:58 +0300

    init

---
+++ src/components/pages/people-page.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { PersonDetails, PersonList } from '../sw-components';
+import Row from '../row';
+import { withRouter } from 'react-router-dom';
+
+const PeoplePage = ({ history, match }) => {
+  const { id } = match.params;
+  return (
+    <Row
+      left={<PersonList onItemSelected={(id) => history.push(id)} />}
+      right={<PersonDetails itemId={id} />}
+    />
+  );
+};
+
+export default withRouter(PeoplePage);
